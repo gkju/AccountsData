935ee3c06894d46e84eb6a51c2845f628dea4610
gkjus <gkjuszczyk@gmail.com>
2021-11-07 15:18:27 +0100

    Up

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,8 +1,4 @@
 ﻿using AccountsData.Models.DataModels;
-using AccountsData.Models.DataModels.Abstracts;
-using AccountsData.Models.DataModels.Implementations.Properties;
-using AccountsData.Models.DataModels.Implementations.Roles;
-using AccountsData.Models.DataModels.Implementations.RoleScope;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,39 +6,12 @@ namespace AccountsData.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Role> BoardsRoles { get; set; }
         public DbSet<Board> Boards { get; set; }
         public DbSet<Thread> Threads { get; set; }
-        public DbSet<Scope> Scopes { get; set; }
         public DbSet<File> Files { get; set; }
         public ApplicationDbContext(
             DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            //all derived classes are added one by one as ef core entities
-            builder.Entity<AuthorityProperty>().ToTable("AuthorityProperties");
-            builder.Entity<BannedProperty>().ToTable("BannedProperties");
-            builder.Entity<MayManageRolesProperty>().ToTable("MayManageRoleProperties");
-            builder.Entity<MemberProperty>().ToTable("MemberProperties");
-            builder.Entity<Prefixes>().ToTable("Prefixes");
-            builder.Entity<EditOrDeleteProperty>().ToTable("EditOrDeleteProperty");
-            builder.Entity<ReadProperty>().ToTable("ReadProperty");
-            builder.Entity<ViewProperty>().ToTable("ViewProperty");
-            builder.Entity<WriteProperty>().ToTable("WriteProperty");
-            builder.Entity<AdminProperty>().ToTable("AdminProperty");
-            builder.Entity<BanUsersProperty>().ToTable("BanUsersProperty");
-            builder.Entity<BannedRole>();
-            builder.Entity<BoardRole>();
-            builder.Entity<GlobalRole>();
-            builder.Entity<BoardScope>();
-            builder.Entity<GlobalBoardScope>();
-            builder.Entity<GlobalScope>();
-            builder.Entity<NewsScope>();
-
-            base.OnModelCreating(builder);
         }
     }
 }
--- Models/DataModels/Thread.cs
+++ Models/DataModels/Thread.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using AccountsData.Models.DataModels.Helpers;
-using AccountsData.Models.DataModels.Implementations.Properties;
 
 namespace AccountsData.Models.DataModels
 {
@@ -20,40 +18,5 @@ namespace AccountsData.Models.DataModels
         
         [Required]
         public string Desc { get; set; }
-        
-        [Required]
-        public bool inheritProperties { get; set; }
-
-        [Required] public Properties defaultProperties { get; set; } = new Properties();
-
-        [Required] public Properties memberProperties { get; set; } = new Properties();
-        
-        public Properties GetProperties(ApplicationUser user)
-        {
-            if (inheritProperties)
-            {
-                return board.GetProperties(user);
-            }
-            else
-            {
-                Properties userProperties = user.GetProperties(this.board.scope);
-                if (userProperties.ContainsPropertyCalled(MemberProperty.Name) && (SimpleBoolProperty) userProperties[MemberProperty.Name])
-                {
-                    foreach (var property in memberProperties)
-                    {
-                        userProperties.InsertOrMerge(property);
-                    }
-                }
-                else
-                {
-                    foreach (var property in defaultProperties)
-                    {
-                        userProperties.InsertOrMerge(property);
-                    }
-                }
-
-                return userProperties;
-            }
-        }
     }
 }
