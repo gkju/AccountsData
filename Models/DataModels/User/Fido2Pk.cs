3e1ca079edf8b0fc8250bb883419c40846667346
gjus <gkjuszczyk@gmail.com>
2022-04-03 00:41:04 +0200

    Fixed openapi gen, missing attr and wrong import

--- Models/DataModels/User/Fido2Pk.cs
+++ Models/DataModels/User/Fido2Pk.cs
@@ -1,9 +1,13 @@
-﻿using Fido2NetLib.Objects;
+﻿using System.ComponentModel.DataAnnotations;
+using Fido2NetLib.Objects;
 
 namespace AccountsData.Models.DataModels;
 
 public class Fido2Pk
 {
+    [Key]
+    public string Id { get; set; }
+    
     public string OwnerId { get; set; }
     public ApplicationUser Owner { get; set; }
     
